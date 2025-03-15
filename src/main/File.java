70e8d51036f533d08f1301b2d74e8dabd4e4d5a5
larabatista8 <larasantosb8@gmail.com>
2025-03-15 12:41:12 -0300

    add menu

--- src/main/File.java
+++ src/main/File.java
@@ -1,10 +1,24 @@
 package main;
+import java.util.Scanner;
 
 public class File {
+	
+	public static void showMenu() {
+		
+		System.out.println("1. Cadastrar um novo pet\r\n"
+				+ "2. Alterar os dados do pet cadastrado\r\n"
+				+ "3. Deletar um pet cadastrado\r\n"
+				+ "4. Listar todos os pets cadastrados\r\n"
+				+ "5. Listar pets por algum critério (idade, nome, raça)\r\n"
+				+ "6. Sair");
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int option;
+		
+		showMenu();
+		
 	}
 
 }
